e19de2104bc27d2dc1c7d0c2d5b998e6728324ac
Samara Grant <samara.amanda01@gmail.com>
2020-11-28 08:09:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,36 @@ class ApplicationController < Sinatra::Base
     Post.create(params)
     redirect '/posts'
   end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params["id"])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params["id"])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    id = params["id"]
+    new_params = {}
+    old_post = Post.find(id)
+    new_params[:name] = params["name"]
+    new_params[:content] = params["content"]
+    old_post.update(new_params)
+
+    redirect "/posts/#{id}"
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params["id"])
+    @post.destroy
+    erb :delete
+  end
 end
